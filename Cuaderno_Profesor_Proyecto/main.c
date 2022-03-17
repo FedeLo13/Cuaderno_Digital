4286eac5a4c7df6dcdfe7f0c73a0bc1b8aab681a
PaTz52 <pablo.troncosozambrano@alum.uca.es>
2022-03-17 17:49:51 +0100

    Implementacion del log in en el modulo main

--- Cuaderno_Profesor_Proyecto/main.c
+++ Cuaderno_Profesor_Proyecto/main.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <"Usuario.h">
+#define N 6
+#defien n 9
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    char U[N], c[n], ad[13];
+    int t, p, nu;
+
+    ad = "administrador";
+    nu = cuentausuarios();
+
+    do{
+        printf("Usuario: \n");
+        fgets(U, N, stdin);
+        prinf("Contraseña: \n");
+        fgets(c, n, stdin);
+        for(i = 0; i < nu || t == 1; i++){
+            if(strcmp(U, v_usuarios[i].Usuario) == 0){
+                t = 1;
+            }else{
+                t = 0;
+            }
+        }
+        for(i = 0; i < nu || p == 1; i++){
+            if(strcmp(c, v_usuarios[i].Contraseña) == 0){
+                p = 1;
+            }else{
+                p = 0;
+            }
+        }
+    }while(t != 1 && p != 1);
+
+    if(strcmp(ad, v_usuarios[i].Perfil_usuario) == 0){
+
+    }else{
+
+    }
 }
