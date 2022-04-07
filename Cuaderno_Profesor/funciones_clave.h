fed809aef8cac528e4b0f2204f64a760c59085d0
heisenbergg23 <miguelangel.durantejero@alum.uca.es>
2022-04-07 16:24:35 +0200

    a

--- Cuaderno_Profesor/funciones_clave.h
+++
@@ -1,9 +0,0 @@
-#ifndef _FUNCIONES_CLAVE_H_
-#define _FUNCIONES_CLAVE_H_
-
-#include <stdio.h>
-#include <string.h>
-
-void vaciar(char *s, int N);
-
-#endif
--- Cuaderno_Profesor/matriculas.c
+++
@@ -1,6 +0,0 @@
-<<<<<<< Updated upstream
-=======
-
-}
->>>>>>> Stashed changes
-
