a7f8dac0f35268330acb8a9f9ede72bbade43ea6
FedeLo13 <100858687+FedeLo13@users.noreply.github.com>
2022-04-13 15:52:30 +0200

    Okayy

--- Cuaderno_Digital/funciones_clave.c
+++ Cuaderno_Digital/funciones_clave.c
@@ -1,1 +1,8 @@
 #include "funciones_clave.h"
+
+void vaciar(char *s, int n){
+    int i;
+    for(i=0;i<n;i++){
+        s[i]= '\0';
+    }
+}
--- Cuaderno_Digital/usuarios.c
+++ Cuaderno_Digital/usuarios.c
@@ -1,2 +1,54 @@
 #include "usuarios.h"
-#include "funciones_clave.h"
+#include "funciones_clave.c"
+
+typedef struct{
+    char Id_usuario[50];
+    char Nomb_usuario[50];
+    char Perfil_usuario[50];
+    char Usuario[50];
+    char Contrasena[50];
+    }Usuario;
+
+Usuario *v_usuarios;
+
+
+int main(){
+    carga_usuarios();
+    return 0;
+}
+
+//cabecera: void carga_usuarios();
+//precondición:
+//poscondición: carga los datos del fichero en la estructura del programa
+void carga_usuarios(){
+
+    int cont=0,i,j;
+    char temp[100],aux[100];
+    FILE *f;
+    f = fopen("usuarios.txt","r");
+    if(f == NULL){
+        printf("Error al abrir el fichero.\n");
+    }
+
+    while(!feof(f)){
+        fgets(temp,100,f);
+        cont++;
+    }
+
+    rewind(f);
+
+    v_usuarios = (Usuario*)malloc(cont*sizeof(Usuario));
+    if(v_usuarios == NULL){
+        printf("No se ha podido reservar la memoria.\n");
+    }
+
+    for(i=0; i<1; i++){
+            vaciar(temp,100);
+        for(j=0;v_usuarios[i].Id_usuario[j]=='-';j++){
+            v_usuarios[i].Id_usuario[j]= fgetc(f);
+        }
+        v_usuarios[i].Id_usuario[j-1]='\0';
+        printf("%s",v_usuarios[i].Id_usuario);
+    }
+    fclose(f);
+}
