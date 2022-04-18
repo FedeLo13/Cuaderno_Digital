b1a7d8773165b2cc4e3e4973d27a4f07b455f08f
heisenbergg23 <miguelangel.durantejero@alum.uca.es>
2022-04-18 22:42:07 +0200

    a

---
+++ Cuaderno_Digital/Horarios.c
@@ -0,0 +1,324 @@
+#include "horario.h"
+#include "funciones_clave.c"
+
+int main(){
+    int n;
+    carga_horario();
+    crea_horario();
+    modifica_horario();
+    elimina_horario();
+    lista_horario();
+    return 0;
+}
+
+//cabecera: void carga_horario();
+//precondici�n:
+//poscondici�n: carga los datos del fichero en la estructura del programa
+void carga_horario(){
+
+    int cont=0,i,j,semaforo;
+    char temp[100],*aux;
+    const char s[2] = "-";
+    FILE *f;
+    f = fopen("horario.txt","r");
+    if(f == NULL){
+        printf("Error al abrir el fichero.\n");
+    }
+
+    while(!feof(f)){
+        fgets(temp,100,f);
+        cont++;
+    }
+
+    rewind(f);
+    v_horario = (horario*)malloc(cont*sizeof(horario));
+    if(v_horario == NULL){
+        printf("No se ha podido reservar la memoria.\n");
+    }
+    for(i=0;i<cont;i++){
+        j=1;
+        vaciar(temp,100);
+        fgets(temp,100,f);
+        aux = strtok(temp,s);
+        while(aux != NULL){
+            switch (j){
+                case 1:
+                    strcpy(v_horario[i].Id_profesor,aux);
+                    break;
+                case 2:
+                    strcpy(v_horario[i].Dia_clase,aux);
+                    break;
+                case 3:
+                    strcpy(v_horario[i].Hora_clase,aux);
+                    break;
+                case 4:
+                    strcpy(v_horario[i].Id_materia,aux);
+                    break;
+                case 5:
+                    strcpy(v_horario[i].Grupo,aux);
+                    break;
+            }
+            aux = strtok(NULL,s);
+            j++;
+        }
+    }
+    for(i=0;i<cont;i++){
+        j=0;
+        semaforo=0;
+        while(semaforo==0){
+            j++;
+            if(v_horario[i].Grupo[j] == '\n'){
+                v_horario[i].Grupo[j] = '\0';
+                semaforo=1;
+            }
+        }
+    }
+    fclose(f);
+}
+
+void lista_horario(){
+    int cont=0,i;
+    char temp[100];
+    FILE *f;
+    f = fopen("horario.txt","r");
+    if(f == NULL){
+        printf("Error al abrir el fichero.\n");
+    }
+
+    while(!feof(f)){
+        fgets(temp,100,f);
+        cont++;
+    }
+    for(i=0;i<cont;i++){
+        printf("%i. %s %s %s\n",i+1,v_hoarario[i].Id_profesor,v_horario[i].Nomb_usuario,v_usuarios[i].Perfil_usuario);
+    }
+    fclose(f);
+}
+
+void modifica_usuarios(){
+    int cont=0,n;
+    char temp[100],c,aux[100];
+    lista_usuarios();
+    FILE *f;
+    f = fopen("usuarios.txt","r");
+    if(f == NULL){
+        printf("Error al abrir el fichero.\n");
+    }
+
+    while(!feof(f)){
+        fgets(temp,100,f);
+        cont++;
+    }
+
+    do{
+        printf("Introduce el numero correspondiente al usuario que desea editar\n");
+        scanf("%i",&n);
+        fflush(stdin);
+    }while(n<=0 || n>cont);
+    n--;
+    do{
+        printf("Escriba el nuevo identificador del usuario (maximo 3 digitos), o escriba x si no quiere editarlo\n");
+        gets(aux);
+        fflush(stdin);
+        if(strcmp(aux,"x")!=0){
+            strcpy(v_usuarios[n].Id_usuario,aux);
+        }
+    }while(strlen(v_usuarios[n].Id_usuario) > 3);
+    do{
+        printf("Escriba el nuevo nombre (maximo 20 caracteres), o escriba x si no quiere editarlo\n");
+        gets(aux);
+        fflush(stdin);
+        if(strcmp(aux,"x")!=0){
+            strcpy(v_usuarios[n].Nomb_usuario,aux);
+        }
+    }while(strlen(v_usuarios[n].Nomb_usuario) > 20);
+    do{
+        printf("Escriba el nuevo nombre de usuario(maximo 5 caracteres), o escriba x si no quiere editarlo\n");
+        gets(aux);
+        fflush(stdin);
+        if(strcmp(aux,"x")!=0){
+            strcpy(v_usuarios[n].Usuario,aux);
+        }
+    }while(strlen(v_usuarios[n].Usuario) > 5);
+    do{
+        printf("Escriba la nueva contrase�a(maximo 8 caracteres), o escriba x si no quiere editarlo\n");
+        gets(aux);
+        fflush(stdin);
+        if(strcmp(aux,"x")!=0){
+            strcpy(v_usuarios[n].Contrasena,aux);
+        }
+    }while(strlen(v_usuarios[n].Contrasena) > 8);
+    do{
+    printf("Esta seguro/a de que quiere realizar estos cambios? (s/n)\n");
+    scanf("%c",&c);
+    fflush(stdin);
+    if(c == 'n'){
+        vaciar(v_usuarios[n].Id_usuario,strlen(v_usuarios[n].Id_usuario));
+        vaciar(v_usuarios[n].Nomb_usuario,strlen(v_usuarios[n].Nomb_usuario));
+        vaciar(v_usuarios[n].Perfil_usuario,strlen(v_usuarios[n].Perfil_usuario));
+        vaciar(v_usuarios[n].Usuario,strlen(v_usuarios[n].Usuario));
+        vaciar(v_usuarios[n].Contrasena,strlen(v_usuarios[n].Contrasena));
+    }
+    }while(c != 'n' && c!='s');
+    fclose(f);
+}
+
+
+void crea_usuarios(){
+    int n,cont=0,i,v=1;
+    char c,temp[100];
+    FILE *f;
+    f = fopen("usuarios.txt","r");
+    if(f == NULL){
+        printf("Error al abrir el fichero.\n");
+    }
+
+    while(!feof(f)){
+        fgets(temp,100,f);
+        cont++;
+    }
+    i=cont;
+    cont++;
+    v_usuarios=(Usuario*)realloc(v_usuarios,cont*sizeof(Usuario));
+    do{
+        printf("Introduzca 1 para crear un profesor, 2 para crear un administrador\n");
+        scanf("%i",&n);
+        fflush(stdin);
+        if(n==1){
+            char prof[]={'p','r','o','f','e','s','o','r','\0'};
+            strcpy(v_usuarios[i].Perfil_usuario,prof);
+        }
+        else{
+            if(n==2){
+                char adm[]= {'a','d','m','i','n','i','s','t','r','a','d','o','r','\0'};
+                strcpy(v_usuarios[i].Perfil_usuario,adm);
+            }
+        }
+    }while(n != 1 && n != 2);
+    do{
+        printf("Introduzca el identificador del %s (maximo 3 digitos)\n",v_usuarios[i].Perfil_usuario);
+        gets(v_usuarios[i].Id_usuario);
+        fflush(stdin);
+    }while(strlen(v_usuarios[i].Id_usuario) > 3);
+    do{
+        printf("Introduzca el nombre del %s (maximo 20 caracteres)\n",v_usuarios[i].Perfil_usuario);
+        gets(v_usuarios[i].Nomb_usuario);
+        fflush(stdin);
+    }while(strlen(v_usuarios[i].Nomb_usuario) > 20);
+    do{
+        printf("Introduzca el nombre de usuario del %s (maximo 5 caracteres)\n",v_usuarios[i].Perfil_usuario);
+        gets(v_usuarios[i].Usuario);
+        fflush(stdin);
+    }while(strlen(v_usuarios[i].Usuario) > 5);
+    do{
+        printf("Introduzca la contrasena del %s (maximo 8 caracteres)\n",v_usuarios[i].Perfil_usuario);
+        gets(v_usuarios[i].Contrasena);
+        fflush(stdin);
+    }while(strlen(v_usuarios[i].Contrasena) > 8);
+    do{
+    printf("Esta seguro/a de que quiere realizar esta operacion? (s/n)\n");
+    scanf("%c",&c);
+    fflush(stdin);
+    if(c == 'n'){
+        vaciar(v_usuarios[i].Id_usuario,strlen(v_usuarios[i].Id_usuario));
+        vaciar(v_usuarios[i].Nomb_usuario,strlen(v_usuarios[i].Nomb_usuario));
+        vaciar(v_usuarios[i].Perfil_usuario,strlen(v_usuarios[i].Perfil_usuario));
+        vaciar(v_usuarios[i].Usuario,strlen(v_usuarios[i].Usuario));
+        vaciar(v_usuarios[i].Contrasena,strlen(v_usuarios[i].Contrasena));
+        v_usuarios=(Usuario*)realloc(v_usuarios,i*sizeof(Usuario));
+        v=0;
+    }
+    }while(c != 'n' && c!='s');
+    fclose(f);
+    vuelca_usuarios(v);
+}
+
+void elimina_usuarios(){
+    int cont=0,n,i=0,admns=0,j,v=-1;
+    char temp[100];
+    lista_usuarios();
+    FILE *f;
+    f = fopen("usuarios.txt","r");
+    if(f == NULL){
+        printf("Error al abrir el fichero.\n");
+    }
+
+    while(!feof(f)){
+        fgets(temp,100,f);
+        cont++;
+    }
+    do{
+        printf("Introduce el numero correspondiente al usuario que desea eliminar\n");
+        scanf("%i",&n);
+        fflush(stdin);
+    }while(n<=0 || n>cont);
+    n--;
+    cont--;
+    if (strcmp(v_usuarios[n].Perfil_usuario,"administrador") == 0){
+        while(i<cont && admns < 2){
+            if(strcmp(v_usuarios[i].Perfil_usuario,"administrador") == 0){
+                admns++;
+            }
+            i++;
+        }
+    }
+    if(admns == 1){
+        printf("No se puede realizar esta accion debido a que solo existe 1 administrador\n");
+        v=0;
+    }
+    else{
+        vaciar(v_usuarios[n].Id_usuario,strlen(v_usuarios[n].Id_usuario));
+        vaciar(v_usuarios[n].Nomb_usuario,strlen(v_usuarios[n].Nomb_usuario));
+        vaciar(v_usuarios[n].Perfil_usuario,strlen(v_usuarios[n].Perfil_usuario));
+        vaciar(v_usuarios[n].Usuario,strlen(v_usuarios[n].Usuario));
+        vaciar(v_usuarios[n].Contrasena,strlen(v_usuarios[n].Contrasena));
+        if(n<cont){
+            for(j=n;j<cont;j++){
+                strcpy(v_usuarios[j].Id_usuario,v_usuarios[j+1].Id_usuario);
+                vaciar(v_usuarios[j+1].Id_usuario,strlen(v_usuarios[j+1].Id_usuario));
+                strcpy(v_usuarios[j].Nomb_usuario,v_usuarios[j+1].Nomb_usuario);
+                vaciar(v_usuarios[j+1].Nomb_usuario,strlen(v_usuarios[j+1].Nomb_usuario));
+                strcpy(v_usuarios[j].Perfil_usuario,v_usuarios[j+1].Perfil_usuario);
+                vaciar(v_usuarios[j+1].Perfil_usuario,strlen(v_usuarios[j+1].Perfil_usuario));
+                strcpy(v_usuarios[j].Usuario,v_usuarios[j+1].Usuario);
+                vaciar(v_usuarios[j+1].Usuario,strlen(v_usuarios[j+1].Usuario));
+                strcpy(v_usuarios[j].Contrasena,v_usuarios[j+1].Contrasena);
+                vaciar(v_usuarios[j+1].Contrasena,strlen(v_usuarios[j+1].Contrasena));
+            }
+            v_usuarios=(Usuario*)realloc(v_usuarios,cont*sizeof(Usuario));
+         }
+         else{
+            v_usuarios=(Usuario*)realloc(v_usuarios,cont*sizeof(Usuario));
+         }
+    }
+    fclose(f);
+    vuelca_usuarios(v);
+}
+
+void vuelca_horario(int n){
+    int cont=0,i;
+    char temp[100];
+    FILE *f;
+    f = fopen("hoario.txt","r");
+    if(f == NULL){
+        printf("Error al abrir el fichero.\n");
+    }
+
+    while(!feof(f)){
+        fgets(temp,100,f);
+        cont++;
+    }
+    fclose(f);
+    cont= cont + n;
+    f = fopen("horario.txt","w");
+    if(f == NULL){
+      printf("Error al abrir el fichero.\n");
+    }
+    for(i=0;i<cont;i++){
+        fprintf(f,"%s-%s-%s-%s-%s",v_horario[i].Id_profesor,v_horario[i].Dia_clase,v_horario[i].Hora_clase,v_horario[i].Id_materia,v_horario[i].Grupo);
+        if(i != (cont-1)){
+            fprintf(f,"\n");
+        }
+    }
+    fclose(f);
+}
--- Cuaderno_Digital/MATRICULA.c
+++ Cuaderno_Digital/MATRICULA.c
@@ -1,110 +1,257 @@
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <locale.h>
-
-void list();
-void vaciar(char *s, int n){
-    int i;
-    for(i=0;i<n;i++){
-        s[i]= '\0';
-    }
-}
-void carga_matriculas();
-int i, j, cont;
-
-typedef struct{
-  char id_alum[6];
-  char id_materia[4];
-
-}Matriculas;
-
-Matriculas *matr;
-int l;
+#include "matricula.h"
+#include "funciones_clave.c"
 
 int main(){
-  carga_matriculas();
-  printf("%s\n", matr[0].id_alum);
-  printf("%s\n", matr[0].id_materia);
-  return 0;
+    int n;
+    carga_matricula();
+    crea_matricula();
+    modifica_matricula();
+    elimina_matricula();
+    lista_matricula();
+    return 0;
 }
-void carga_matriculas(){
-char p[100], *token;
-const char s[2] = "-";
-   FILE*f;
-   f = fopen("materia.txt","r");
+void carga_matricula(){
 
- if(f == NULL){
-    printf("no se ha podido abrir el fichero. \n");
-    exit(1);
-   }
+    int cont=0,i,j,semaforo;
+    char temp[100],*aux;
+    const char s[2] = "-";
+    FILE *f;
+    f = fopen("matriculas.txt","r");
+    if(f == NULL){
+        printf("Error al abrir el fichero.\n");
+    }
 
-while(!feof(f)){
-    fgets(p, 100, f);
+    while(!feof(f)){
+        fgets(temp,100,f);
+        cont++;
+    }
+
+    rewind(f);
+    v_matricula = (matricula*)malloc(cont*sizeof(matricula));
+    if(v_matricula == NULL){
+        printf("No se ha podido reservar la memoria.\n");
+    }
+    for(i=0;i<cont;i++){
+        j=1;
+        vaciar(temp,100);
+        fgets(temp,100,f);
+        aux = strtok(temp,s);
+        while(aux != NULL){
+            switch (j){
+                case 1:
+                    strcpy(v_matricula[i].Id_materia,aux);
+                    break;
+                case 2:
+                    strcpy(v_matricula[i].Id_alum,aux);
+                    break;
+            }
+            aux = strtok(NULL,s);
+            j++;
+        }
+    }
+
+    fclose(f);
+}
+
+void lista_matricula(){
+    int cont=0,i;
+    char temp[100];
+    FILE *f;
+    f = fopen("matriculas.txt","r");
+    if(f == NULL){
+        printf("Error al abrir el fichero.\n");
+    }
+
+    while(!feof(f)){
+        fgets(temp,100,f);
+        cont++;
+    }
+    for(i=0;i<cont;i++){
+        printf("%i. %s %s %s\n",i+1,v_matricula[i].Id_materia,v_matricula[i].Id_alum);
+    }
+    fclose(f);
+}
+
+void modifica_matricula(){
+    int cont=0,n;
+    char temp[100],c,aux[100];
+    lista_usuarios();
+    FILE *f;
+    f = fopen("matriculas.txt","r");
+    if(f == NULL){
+        printf("Error al abrir el fichero.\n");
+    }
+
+    while(!feof(f)){
+        fgets(temp,100,f);
+        cont++;
+    }
+
+    do{
+        printf("Introduce el numero correspondiente a la matricula que desea editar\n");
+        scanf("%i",&n);
+        fflush(stdin);
+    }while(n<=0 || n>cont);
+    n--;
+    do{
+        printf("Escriba el nuevo identificador de materia (maximo 4 digitos), o escriba x si no quiere editarlo\n");
+        gets(aux);
+        fflush(stdin);
+        if(strcmp(aux,"x")!=0){
+            strcpy(v_matricula[n].Id_materia,aux);
+        }
+    }while(strlen(v_usuarios[n].Id_materia) > 3);
+    do{
+        printf("Escriba el nuevo nombre (maximo 20 caracteres), o escriba x si no quiere editarlo\n");
+        gets(aux);
+        fflush(stdin);
+        if(strcmp(aux,"x")!=0){
+            strcpy(v_matricula[n].Id_alum,aux);
+        }
+    }do{
+    printf("Esta seguro/a de que quiere realizar estos cambios? (s/n)\n");
+    scanf("%c",&c);
+    fflush(stdin);
+    if(c == 'n'){
+        vaciar(v_horario[n].Id_materia,strlen(v_horario[n].Id_materia));
+        vaciar(v_horario[n].Id_alum,strlen(v_horario[n].Id_alum));
+    }
+    }while(c != 'n' && c!='s');
+    fclose(f);
+}
+
+
+void crea_matricula(){
+    int n,cont=0,i,v=1;
+    char c,temp[100];
+    FILE *f;
+    f = fopen("matriculas.txt","r");
+    if(f == NULL){
+        printf("Error al abrir el fichero.\n");
+    }
+
+    while(!feof(f)){
+        fgets(temp,100,f);
+        cont++;
+    }
+    i=cont;
     cont++;
-}
-
-l = cont;
-
-matr = (Matriculas*)malloc(l*sizeof(Matriculas));
-
-
-if(matr == NULL) {
-    printf("Error al reservar memoria.");
-}
-
-rewind(f);
-
-for(i == 0; i < l; i++){
-	vaciar(p, 100);
-	fgets(p, 100, f);
-	token = strtok(p, s);
-	j = 0;
-
-while(token != NULL){
-		switch(j){
-        	case 0: strcpy(matr[i].id_alum, token);
-        	break;
-            case 1: strcpy(matr[i].id_materia, token);
-            break;
+    v_matricula=(Matricula*)realloc(v_matricula,cont*sizeof(Matricula));
+    do{
+        printf("Introduzca el identificador del %s (maximo 3 digitos)\n",v_usuarios[i].Perfil_usuario);
+        gets(v_usuarios[i].Id_usuario);
+        fflush(stdin);
+    }while(strlen(v_usuarios[i].Id_usuario) > 3);
+    do{
+        printf("Introduzca el nombre del %s (maximo 20 caracteres)\n",v_usuarios[i].Perfil_usuario);
+        gets(v_usuarios[i].Nomb_usuario);
+        fflush(stdin);
+    }while(strlen(v_usuarios[i].Nomb_usuario) > 20);
+    do{
+        printf("Introduzca el nombre de usuario del %s (maximo 5 caracteres)\n",v_usuarios[i].Perfil_usuario);
+        gets(v_usuarios[i].Usuario);
+        fflush(stdin);
+    }while(strlen(v_usuarios[i].Usuario) > 5);
+    do{
+        printf("Introduzca la contrasena del %s (maximo 8 caracteres)\n",v_usuarios[i].Perfil_usuario);
+        gets(v_usuarios[i].Contrasena);
+        fflush(stdin);
+    }while(strlen(v_usuarios[i].Contrasena) > 8);
+    do{
+    printf("Esta seguro/a de que quiere realizar esta operacion? (s/n)\n");
+    scanf("%c",&c);
+    fflush(stdin);
+    if(c == 'n'){
+        vaciar(v_usuarios[i].Id_usuario,strlen(v_usuarios[i].Id_usuario));
+        vaciar(v_usuarios[i].Nomb_usuario,strlen(v_usuarios[i].Nomb_usuario));
+        vaciar(v_usuarios[i].Perfil_usuario,strlen(v_usuarios[i].Perfil_usuario));
+        vaciar(v_usuarios[i].Usuario,strlen(v_usuarios[i].Usuario));
+        vaciar(v_usuarios[i].Contrasena,strlen(v_usuarios[i].Contrasena));
+        v_usuarios=(Usuario*)realloc(v_usuarios,i*sizeof(Usuario));
+        v=0;
     }
-		token = strtok(NULL, s);
-		j++;
-  }
+    }while(c != 'n' && c!='s');
+    fclose(f);
+    vuelca_usuarios(v);
 }
-fclose(f);
 
-void list(){
-	int i;
-	char id[6];
-
-	for(i = 0; i < l; i++){
-        printf("%s - ", matr[i].id_alum);
-        printf("%s\n", matr[i].id_materia);
-	}
-
-	printf("Selecciona un id: \n");
-	fgets(id, 6, stdin);
-
-	for(i = 0; i <= l; i++){
-		if(strcoll(id, matr[i].id_alum) == 0){
-			printf("%s\n", matr[i].id_alum);
-			printf("%s\n", matr[i].id_materia);
+void elimina_matricula(){
+    int cont=0,n,i=0,admns=0,j,v=-1;
+    char temp[100];
+    lista_usuarios();
+    FILE *f;
+    f = fopen("matriculas.txt","r");
+    if(f == NULL){
+        printf("Error al abrir el fichero.\n");
     }
-  }
-}
-void end(){
-	FILE *f;
-	int i;
-	f = fopen("alumnos.txt", "w");
-	if(f == NULL){
-		printf("ERROR\n");
-	}
 
-	for(i = 0; i<l; i++){
-	    fprintf(f, "$s-", matr[i].id_alum);
-        fprintf(f, "$s-", matr[i].id_materia);
- }
-	fclose(f);
+    while(!feof(f)){
+        fgets(temp,100,f);
+        cont++;
+    }
+    do{
+        printf("Introduce el numero correspondiente a la matricula que desea eliminar\n");
+        scanf("%i",&n);
+        fflush(stdin);
+    }while(n<=0 || n>cont);
+    n--;
+    cont--;
+    if (strcmp(v_matricula[n].Perfil_usuario,"administrador") == 0){
+        while(i<cont && admns < 2){
+            if(strcmp(v_usuarios[i].Perfil_usuario,"administrador") == 0){
+                admns++;
+            }
+            i++;
+        }
+    }
+    if(admns == 1){
+        printf("No se puede realizar esta accion debido a que solo existe 1 administrador\n");
+        v=0;
+    }
+    else{
+        vaciar(v_matricula[n].Id_materia,strlen(v_matricula[n].Id_materia));
+        vaciar(v_matricula[n].Id_alum,strlen(v_matricula[n].Id_alum));
+        if(n<cont){
+            for(j=n;j<cont;j++){
+                strcpy(v_matricula[j].Id_materia,v_matricula[j+1].Id_materia);
+                vaciar(v_matricula[j+1].Id_materia,strlen(v_matricula[j+1].Id_materia));
+                strcpy(v_matricula[j].Id_alum,v_matricula[j+1].Id_alum);
+                vaciar(v_matricula[j+1].Id_alum,strlen(v_usuarios[j+1].Id_alum));
+            }
+            v_matricula=(Matricula*)realloc(v_matricula,cont*sizeof(matricula));
+         }
+         else{
+            v_matricula=(matricula*)realloc(v_matricula,cont*sizeof(matricula));
+         }
+    }
+    fclose(f);
+    vuelca_matricula(v);
+}
+
+void vuelca_matricula(int n){
+    int cont=0,i;
+    char temp[100];
+    FILE *f;
+    f = fopen("matriculas.txt","r");
+    if(f == NULL){
+        printf("Error al abrir el fichero.\n");
+    }
+
+    while(!feof(f)){
+        fgets(temp,100,f);
+        cont++;
+    }
+    fclose(f);
+    cont= cont + n;
+    f = fopen("matriculas.txt","w");
+    if(f == NULL){
+      printf("Error al abrir el fichero.\n");
+    }
+    for(i=0;i<cont;i++){
+        fprintf(f,"%s-%s-%s-%s-%s",v_matricula[i].Id_materia,v_matricula[i].Id_alum);
+        if(i != (cont-1)){
+            fprintf(f,"\n");
+        }
+    }
+    fclose(f);
 }
