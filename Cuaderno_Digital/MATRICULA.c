d296c3ec36f937bea18c4dfad94e556d42e6fc89
heisenbergg23 <miguelangel.durantejero@alum.uca.es>
2022-04-18 11:08:00 +0200

    a

--- Cuaderno_Digital/MATRICULA.c
+++ Cuaderno_Digital/MATRICULA.c
@@ -1,59 +1,109 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <locale.h>
+
+void list();
+void vaciar(char *s, int n){
+    int i;
+    for(i=0;i<n;i++){
+        s[i]= '\0';
+    }
+}
+void carga_matriculas();
+int i, j, cont;
 
 typedef struct{
-    int materia,alumno;
-}matricula;
+  char id_alum[6];
+  char id_materia[4];
 
-matricula *v_matriculas;
+}Matriculas;
 
+Matriculas *matr;
+int l;
+
+int main(){
+  carga_matriculas();
+  printf("%s\n", matr[0].id_alum);
+  printf("%s\n", matr[0].id_materia);
+  return 0;
+}
 void carga_matriculas(){
-
-   int i,j;
-   char aux;
-   char temp[50];
-   int cont = 0;
-   FILE *f;
+char p[100], *token;
+const char s[2] = "-";
+   FILE*f;
    f = fopen("materia.txt","r");
+
  if(f == NULL){
     printf("no se ha podido abrir el fichero. \n");
     exit(1);
    }
-   while (!feof(f)){
-       fgets(temp,50,f);
-       cont++;
-   }
 
-   rewind(f);
-
-   v_matriculas = (matricula*)malloc(cont*sizeof(matricula));
-   if (v_matriculas == NULL){
-      printf("no se ha podido reservar la memoria, \n");
-      exit(1);
-  }
-
-   for (i = 0; !feof(f); i++){
-      vaciar(temp);
-      aux = '0';
-    for (j = 0; aux != '-'; i++);
-        aux = fgetc(f);
-        if (aux != '-'){
-          temp[j] = aux;
-     }
-   }
-   copiar(temp,i);
+while(!feof(f)){
+    fgets(p, 100, f);
+    cont++;
 }
 
-void vaciar(char temp[]){
-   int i;
-    for(i = 0; i<50; i++){
-        temp[i] = '\0';
+l = cont;
+
+matr = (Matriculas*)malloc(l*sizeof(Matriculas));
+
+
+if(matr == NULL) {
+    printf("Error al reservar memoria.");
+}
+
+rewind(f);
+
+for(i == 0; i < l; i++){
+	vaciar(p, 100);
+	fgets(p, 100, f);
+	token = strtok(p, s);
+	j = 0;
+
+while(token != NULL){
+		switch(j){
+        	case 0: strcpy(matr[i].id_alum, token);
+        	break;
+            case 1: strcpy(matr[i].id_materia, token);
+            break;
     }
+		token = strtok(NULL, s);
+		j++;
+  }
 }
-void copiar(char temp[], int i){
-  int N = strlen(temp) +1;
-  materia[i] .alumno = (char*)malloc(N*sizeof (char);
-  if (materia[i].alumno == NULL){
-   }
+fclose(f);
+
+void list(){
+	int i;
+	char id[6];
+
+	for(i = 0; i < l; i++){
+        printf("%s - ", matr[i].id_alum);
+        printf("%s\n", matr[i].id_materia);
+	}
+
+	printf("Selecciona un id: \n");
+	fgets(id, 6, stdin);
+
+	for(i = 0; i <= l; i++){
+		if(strcoll(id, matr[i].id_alum) == 0){
+			printf("%s\n", matr[i].id_alum);
+			printf("%s\n", matr[i].id_materia);
+    }
+  }
+}
+void end(){
+	FILE *f;
+	int i;
+	f = fopen("alumnos.txt", "w");
+	if(f == NULL){
+		printf("ERROR\n");
+	}
+
+	for(i = 0; i<l; i++){
+	    fprintf(f, "$s-", matr[i].id_alum);
+        fprintf(f, "$s-", matr[i].id_materia);
+ }
+	fclose(f);
+}
